d71813e96d3b152f9b1290f9524c53974f26c9f0
Umar <muhammadshafqatali3@gmail.com>
2023-08-13 16:48:17 +0500

    first commit

---
+++ Server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
