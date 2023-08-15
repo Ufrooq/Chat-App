f1f5032860615af9bd3592c765ac1c148ad9046f
Umar <muhammadshafqatali3@gmail.com>
2023-08-15 22:43:04 +0500

    sdfsd

--- chat/src/Components/Pages/Chats.js
+++ chat/src/Components/Pages/Chats.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import avatar from "../assets/avatar.png";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  const [chats, setfirst] = useState([
+    "Sam",
+    "Cam",
+    "Raven",
+    "Jeena",
+    "Waran",
+    "Rock",
+    "Screw",
+    "Balack",
+    "Umar",
+    "Bilal",
+  ]);
+  return (
+    <>
+      <div className="chat-page">
+        <div className="chat-container">
+          <div className="chats-panel">
+            <h1>Duckchats</h1>
+            <div className="chats">
+              {chats.map((chat, index) => (
+                <div className="chat" key={index}>
+                  <img src={avatar} alt="avatar" />
+                  <h3>{chat}</h3>
+                  <p>7:34 PM</p>
+                </div>
+              ))}
+            </div>
+            <div className="admin-panel">
+              <img src={avatar} alt="avatar" />
+              <h2>ADMIN</h2>
+            </div>
+          </div>
+          <div className="chat-box">
+            <div className="chat-head">
+              <div className="user-details">
+                <img src={avatar} alt="avatar" />
+                <h2>UserName</h2>
+              </div>
+              <button>Logout</button>
+            </div>
+            <div className="controls">
+              <button>:)</button>
+              <input type="text" placeholder="Message" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Chats;
