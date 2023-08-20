0803fac92fc094284f6d902a11067f88daa8cc1d
Umar <muhammadshafqatali3@gmail.com>
2023-08-20 16:25:22 +0500

    sdssd

--- index.js
+++
@@ -1,6 +0,0 @@
-const sender = "7878787878787";
-const reciever = "5656565656565";
-
-const user3_ID = `Room_ID${user1_ID}_${user2_ID}`;
-
-console.log(user3_ID.includes(user1_ID) && user3_ID.includes(user2_ID));
